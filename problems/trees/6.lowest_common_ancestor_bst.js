e99c8235ec3322b60b40e8efc86cd389ce4befd3
gokulkrishh <krishnangokul9@gmail.com>
2020-02-14 15:18:13 +0530

    Update tree problem

--- problems/trees/6.lowest_common_ancestor_bst.js
+++ problems/trees/6.lowest_common_ancestor_bst.js
@@ -1,6 +1,8 @@
 /* Problem: Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
 
-  Note: According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+	Note: According to the definition of LCA on Wikipedia:
+	“The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants
+	(where we allow a node to be a descendant of itself).”
 
   Example:
 
@@ -21,29 +23,37 @@
 // p,q > root then values lies in right side of root.
 // p is on left and q is on right, then root is the answer.
 
-var lowestCommonAncestor = function (root, p, q) {
-  // p, q < root
-  if (p.val < root.val && q.val < root.val) {
-    return lowestCommonAncestor(root.left, p, q);
-  }
-  // p, q > root
-  else if (p.val > root.val && q.val > root.val) {
-    return lowestCommonAncestor(root.right, p, q);
-  }
-  // p, q on left and right of the root
-  else {
-    return root;
-  }
+var lowestCommonAncestor = function(root, p, q) {
+	// p, q < root
+	if (p.val < root.val && q.val < root.val) {
+		return lowestCommonAncestor(root.left, p, q);
+	}
+	// p, q > root
+	else if (p.val > root.val && q.val > root.val) {
+		return lowestCommonAncestor(root.right, p, q);
+	}
+	// p, q on left and right of the root
+	else {
+		return root;
+	}
 };
 
 var root = {
-  val: 6,
-  left: {
-    val: 2, left: { val: 0, right: null, left: null }, right: { val: 4, right: { val: 5, right: null, left: null }, left: { val: 3, right: null, left: null } }
-  },
-  right: {
-    val: 8, right: { left: null, val: 7, right: null }, left: { left: null, val: 9, right: null }
-  }
+	val: 6,
+	left: {
+		val: 2,
+		left: { val: 0, right: null, left: null },
+		right: {
+			val: 4,
+			right: { val: 5, right: null, left: null },
+			left: { val: 3, right: null, left: null }
+		}
+	},
+	right: {
+		val: 8,
+		right: { left: null, val: 7, right: null },
+		left: { left: null, val: 9, right: null }
+	}
 };
 
 var p = { val: 2, left: null, right: null };
--- problems/trees/7.lowest_common_ancestor_bt.js
+++ problems/trees/7.lowest_common_ancestor_bt.js
@@ -1,6 +1,7 @@
 /* Problem: Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the BT.
 
-  Note: According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+	Note: According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w
+	as the lowest node in T that has both v and w as descendants (we can allow a node to be a descendant of itself too).”
 
   Example:
 
@@ -12,7 +13,8 @@
          /  \
          3   5
 
-  Output: the lowest common ancestor (LCA) of nodes 2 and 8 is 6. Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+	Output: the lowest common ancestor (LCA) of nodes 2 and 8 is 6.
+	Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
 
 */
 
@@ -23,23 +25,36 @@
 // if both left & right is found return root as both p & q resides on left and right of root;
 // else only left tree is found, return left tree or right tree (As per note)
 
-var lowestCommonAncestor = function (root, p, q) {
-  if (!root) return;
-  if (p.val === root.val || q.val === root.val) return root;
-  var leftTree = lowestCommonAncestor(root.left, p, q);
-  var rightTree = lowestCommonAncestor(root.right, p, q);
-  if (leftTree && rightTree) return root;
-  return leftTree ? leftTree : rightTree;
+var lowestCommonAncestor = function(root, p, q) {
+	if (!root) return;
+
+	if (p.val === root.val || q.val === root.val) return root;
+
+	var leftTree = lowestCommonAncestor(root.left, p, q);
+
+	var rightTree = lowestCommonAncestor(root.right, p, q);
+
+	if (leftTree && rightTree) return root;
+
+	return leftTree ? leftTree : rightTree;
 };
 
 var root = {
-  val: 6,
-  left: {
-    val: 2, left: { val: 0, right: null, left: null }, left: { val: 4, right: { val: 5, right: null, left: null }, left: { val: 3, right: null, left: null } }
-  },
-  right: {
-    val: 8, right: { left: null, val: 7, right: null }, left: { left: null, val: 9, right: null }
-  }
+	val: 6,
+	left: {
+		val: 2,
+		left: { val: 0, right: null, left: null },
+		right: {
+			val: 4,
+			right: { val: 5, right: null, left: null },
+			left: { val: 3, right: null, left: null }
+		}
+	},
+	right: {
+		val: 8,
+		right: { left: null, val: 7, right: null },
+		left: { left: null, val: 9, right: null }
+	}
 };
 
 var p = { val: 2, left: null, right: null };
