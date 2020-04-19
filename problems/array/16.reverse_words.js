3a4d40e5bb42909500f82f97d4deba1bdfa23e8a
gokulkrishh <krishnangokul9@gmail.com>
2020-04-19 20:54:01 +0530

    Updated

--- problems/array/16.reverse_words.js
+++ problems/array/16.reverse_words.js
@@ -26,36 +26,78 @@
 
 // Solution 1 using filter to remove "" string
 var reverseWords = function(s) {
-  if (!s) return '';
-  var strArr = s.split(' ');
-  strArr = strArr.filter(Boolean);
-  var str = '';
-  for (var i = strArr.length - 1; i >= 0; i--) {
-    if (!str) {
-      str += strArr[i];
-    } else {
-      str = str + ' ' + strArr[i];
-    }
-  }
+	if (!s) return "";
+	var strArr = s.split(" ");
+	strArr = strArr.filter(Boolean);
+	var str = "";
+	for (var i = strArr.length - 1; i >= 0; i--) {
+		if (!str) {
+			str += strArr[i];
+		} else {
+			str = str + " " + strArr[i];
+		}
+	}
 
-  return str;
+	return str;
 };
 
-reverseWords('the sky is blue'); // "blue is sky the"
-reverseWords('  hello world!  '); // "world! hello"
+reverseWords("the sky is blue"); // "blue is sky the"
+reverseWords("  hello world!  "); // "world! hello"
 
 // Time Complexity: O(N)
-// Space Complexity; O(1)
+// Space Complexity; O(N)
 
 // Solution 2 using replace, trim
-var reverseWords = function(s) [
-  if (!s) return ""
-  var sArr = s.replace(/\s+/g, ' ').trim().split(' ');
-  return sArr.reverse().join(' ');
-]
+var reverseWords = function(s) {
+	if (!s) return "";
+	var sArr = s
+		.replace(/\s+/g, " ")
+		.trim()
+		.split(" ");
+	return sArr.reverse().join(" ");
+};
 
-reverseWords('the sky is blue'); // "blue is sky the"
-reverseWords('  hello world!  '); // "world! hello"
+reverseWords("the sky is blue"); // "blue is sky the"
+reverseWords("  hello world!  "); // "world! hello"
+
+// Time Complexity: O(N)
+// Space Complexity; O(N)
+
+// Solution 3 using char reversal
+
+var reverseWords = function(s) {
+	if (!s.length) return "";
+
+	function swap(s, start, end) {
+		while (start < end) {
+			var temp = str[start];
+			str[start] = str[end];
+			str[end] = temp;
+
+			start++;
+			end--;
+		}
+	}
+
+	var start = 0;
+
+	for (var end = 0; end < s.length; end++) {
+		if (s[end] === " ") {
+			swap(s, start, end);
+			start = end + 1;
+		}
+	}
+
+	// In above for - loop we would reversed all chars last word since (last word doesnt have space)
+	// So we will do now
+	swap(s, start, s.length - 1); // To swap last word chars
+
+	swap(s, 0, s.length - 1); // To swap all reversed chars words
+
+	return s.join("");
+};
+
+reverseWords(s.split(""));
 
 // Time Complexity: O(N)
 // Space Complexity; O(1)
