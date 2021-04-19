060554ad5eb43502a65c31f228f4c02ad0f187d1
gokulkrishh <krishnangokul9@gmail.com>
2021-04-19 17:45:23 +0530

    Update: missing numbers

--- problems/leetcode/array/11.missing_number.js
+++ problems/leetcode/array/11.missing_number.js
@@ -14,18 +14,20 @@
 
 // Solution 1 - Sorting & Finding the number
 var missingNumber = function (arr) {
-  if (arr.length === 1 && arr[0] === 0) return 1;
+	if (arr.length === 1 && arr[0] === 0) return 1;
 
-  arr = arr.sort(function (a, b) { return a - b });
+	arr = arr.sort(function (a, b) {
+		return a - b;
+	});
 
-  if (arr.length === 1 && arr[0] === 1) return 0;
+	if (arr.length === 1 && arr[0] === 1) return 0;
 
-  for (var i = arr[0]; i <= arr[arr.length - 1]; i++) {
-    if (i !== arr[i]) return i;
-    else continue;
-  }
+	for (var i = arr[0]; i <= arr[arr.length - 1]; i++) {
+		if (i !== arr[i]) return i;
+		else continue;
+	}
 
-  return i;
+	return i;
 };
 
 missingNumber([3, 0, 1]); // 2
@@ -36,21 +38,21 @@ missingNumber([3, 0, 1]); // 2
 // Solution 2 - Hashmaps
 
 var missingNumber = function (arr) {
-  if (arr.length === 1 && arr[0] === 0) return 1;
+	if (arr.length === 1 && arr[0] === 0) return 1;
 
-  var obj = {};
+	var obj = {};
 
-  for (var i = 0; i < arr.length; i++) {
-    obj[arr[i]] = 1;
-  }
+	for (var i = 0; i < arr.length; i++) {
+		obj[arr[i]] = 1;
+	}
 
-  for (var i = 0; i < arr.length + 1; i++) {
-    if (!obj[i]) {
-      return i;
-    }
-  }
+	for (var i = 0; i < arr.length + 1; i++) {
+		if (!obj[i]) {
+			return i;
+		}
+	}
 
-  return -1;
+	return -1;
 };
 
 missingNumber([3, 0, 1]); // 2
@@ -59,19 +61,16 @@ missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]); // 8
 // Time Complexity: O(N)
 // Space Complexity: O(N)
 
-
 // Solution - 3 - Using XOR Operation, as number ^ number equal 0
 var missingNumber = function (arr) {
-  if (arr.length === 1 && arr[0] === 0) return 1;
+	var missing = arr.length;
 
-  var missing = arr.length;
+	for (var i = 0; i < arr.length; i++) {
+		missing = i ^ arr[i] ^ missing;
+	}
 
-  for (var i = 0; i < arr.length; i++) {
-    missing ^= i ^ arr[i];
-  }
-
-  return missing;
-}
+	return missing;
+};
 
 missingNumber([3, 0, 1]); // 2
 missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]); // 8
